60c5194a4866fdcfbbcafe5828ef05142b6500e3
marlen <marlen.avila93@gmail.com>
2016-05-19 17:24:23 +0200

    layout afegir obra done en principi

--- app/src/main/java/com/example/marlen/ventaentrades_idi/afegirObra.java
+++ app/src/main/java/com/example/marlen/ventaentrades_idi/afegirObra.java
@@ -2,8 +2,10 @@ package com.example.marlen.ventaentrades_idi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class afegirObra extends AppCompatActivity {
+    Button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
