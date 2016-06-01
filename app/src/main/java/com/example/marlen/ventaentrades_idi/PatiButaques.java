a3255e12846b31fbc5777c0ecd078e9dbaff30d4
marlen <marlen.avila93@gmail.com>
2016-06-01 13:34:30 +0200

    Layout pati de butaques done

--- app/src/main/java/com/example/marlen/ventaentrades_idi/PatiButaques.java
+++ app/src/main/java/com/example/marlen/ventaentrades_idi/PatiButaques.java
@@ -2,12 +2,28 @@ package com.example.marlen.ventaentrades_idi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class PatiButaques extends AppCompatActivity {
+public class PatiButaques extends AppCompatActivity implements View.OnClickListener{
+
+    TextView esc_et, titolEsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pati_butaques);
+
+        esc_et = (TextView)findViewById(R.id.escenari_et);
+        titolEsc = (TextView)findViewById(R.id.esc_titolObra);
+
+        Bundle b = getIntent().getExtras();
+        String titolRec = b.getString("titol");
+        titolEsc.setText(titolRec);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
