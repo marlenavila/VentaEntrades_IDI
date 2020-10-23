3940e5923e1c536ebfe3d1f69549477774e0de91
Adria Navarrete <adri.navarrete.09@gmail.com>
2020-10-23 11:40:45 +0200

    added paraMarlen.js

---
+++ paraMarlen.js
@@ -0,0 +1,13 @@
+/*Hola Marlen! Descubrí tu perfil y me encantó... explicabas que eras Mobile Developer y 
+que te gustaban los unicornios. Me parecio original y un poco loco, pero he decidido escribirte mediante un pull request 
+porque no he querido desaprovechar la oprtunidad de conocerte.
+*/
+
+if(curiosidadPorConocerme === true){
+    instagram = "@adria.navarrete"
+}else{
+    console.log("Solo quería ser original y espero no haberte molestado, un saludo :)")
+}
+
+
+
